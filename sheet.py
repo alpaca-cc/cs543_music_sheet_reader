6ac470eb2cb8101ba078a04684b520844f678ec4
Frank <yshen47@illinois.edu>
2018-04-14 21:08:51 -0500

    created music random generator

---
+++ sheet.py
@@ -0,0 +1,11 @@
+
+class Sheet(object):
+
+	def __init__(self, directory):
+		self.image = 
+
+	def detect_staff_line_center():
+		'''
+			detect staff_line_center in one piece of music sheet
+		'''
+		pass
